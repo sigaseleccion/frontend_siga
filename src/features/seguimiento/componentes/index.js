bdae6e26a5e1d48b2f1382b19988b42f670ad159
Diego Alejandro David Higuita <ddavihig@contratista.epm.co>
2026-02-05 22:02:15 -0500

    Seguimiento 3

--- src/features/seguimiento/componentes/index.js
+++ src/features/seguimiento/componentes/index.js
@@ -1,4 +1,3 @@
 export { default as EditCuotaModal } from './EditCuotaModal';
 export { default as AprendicesIncompletosModal } from './AprendicesIncompletosModal';
 export { default as AprendizDetailModal } from './AprendizDetailModal';
-export { default as EditFechasModal } from './EditFechasModal';
