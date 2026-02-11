3f940919b71e4d81e909c583ecc0894a69987102
imzura <zurangely_portillo@soy.sena.edu.co>
2026-02-11 02:07:21 -0500

    correccion de errores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Nunito", "sans-serif"],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
